c2cbfaf61ed3d78ff6c5459dac64a6e3e9e07460
twikssi <limpbiski15@gmail.com>
2020-08-21 15:50:21 +0300

    show all random numbers and them sum in main

--- src/main/java/by/andry/twikss/App.java
+++ src/main/java/by/andry/twikss/App.java
@@ -1,7 +1,16 @@
 package by.andry.twikss;
 
+import by.andry.twikss.service.RandomGeneratorDemo;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
+        RandomGeneratorDemo randomGeneratorDemo = new RandomGeneratorDemo();
+        logger.info(randomGeneratorDemo.toString());
+        String sum = "Sum all random numbers = " + randomGeneratorDemo.getSumAllNumbers();
+        logger.info(sum);
         //        entry point, to be implemented
     }
 }
