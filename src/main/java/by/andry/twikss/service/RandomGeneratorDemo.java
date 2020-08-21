e9a42909e45e070aed4fe184f461cbc7d6028776
twikssi <limpbiski15@gmail.com>
2020-08-21 14:04:51 +0300

    create getters and auto fill fields with constructor

--- src/main/java/by/andry/twikss/service/RandomGeneratorDemo.java
+++ src/main/java/by/andry/twikss/service/RandomGeneratorDemo.java
@@ -3,9 +3,25 @@ package by.andry.twikss.service;
 public class RandomGeneratorDemo {
     private double a;
     private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
     private double c;
 
     public RandomGeneratorDemo() {
+        this.a = getRandomNumber();
+        this.b = getRandomNumber();
+        this.c = getRandomNumber();
 
     }
 
